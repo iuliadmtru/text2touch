256c378bb7b8df4c64d758db5e871da3407a3971
Ana Mirza <anamirza2002@gmail.com>
2023-03-25 21:11:49 +0200

    changed function parameters

--- util/dalle/dalle_generator.py
+++ util/dalle/dalle_generator.py
@@ -7,13 +7,10 @@ from pathlib import Path
 import openai
 
 
-def d_generate():
-    PROMPT = "An eco-friendly computer from the 90s in the style of vaporwave"
-    # PROMPT = input(); 
+def d_generate(promt):
+    PROMPT = promt
     DATA_DIR = Path.cwd() / "responses"
-
     DATA_DIR.mkdir(exist_ok=True)
-
 
     openai.api_key = os.getenv("OPENAI_API_KEY")
 
@@ -33,5 +30,5 @@ def d_generate():
 
 
 if __name__ == '__main__':
-    # call create func
-    print(d_generate())
+    # call the generation function
+    print(d_generate("An eco-friendly computer from the 90s in the style of vaporwave"))
