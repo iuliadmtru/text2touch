ef0d95a1e77eda23cdce3b6a2ca1dbd1237f3848
Ana Mirza <anamirza2002@gmail.com>
2023-03-25 21:42:18 +0200

    return list of dalle images
    Signed-off-by: Ana Mirza <anamirza2002@gmail.com>

--- util/dalle/dalle_generator.py
+++ util/dalle/dalle_generator.py
@@ -1,6 +1,5 @@
 # dalle_generator.py
 
-import json
 import os
 from pathlib import Path
 
@@ -9,24 +8,29 @@ import openai
 
 def d_generate(promt):
     PROMPT = promt
+    # for debugging
     DATA_DIR = Path.cwd() / "responses"
     DATA_DIR.mkdir(exist_ok=True)
 
     openai.api_key = os.getenv("OPENAI_API_KEY")
 
+    # call dalle to generate images
     response = openai.Image.create(
         prompt=PROMPT,
-        n=1,
+        n=4,
         size="256x256",
         response_format="b64_json",
     )
 
-    file_name = DATA_DIR / f"{PROMPT[:5]}-{response['created']}.json"
+    # uncomment for debugging
+    # file_name = DATA_DIR / f"{PROMPT[:5]}-{response['created']}.json"
 
-    with open(file_name, mode="w", encoding="utf-8") as file:
-        json.dump(response, file)
+    # with open(file_name, mode="w", encoding="utf-8") as file:
+    #     json.dump(response, file)
         
-    return response["data"][0]["b64_json"]
+    list_of_images = [response["data"][0]["b64_json"], response["data"][1]["b64_json"],
+                      response["data"][2]["b64_json"], response["data"][3]["b64_json"]]
+    return list_of_images
 
 
 if __name__ == '__main__':
