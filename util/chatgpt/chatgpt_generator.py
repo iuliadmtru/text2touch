dee27fc2b7ac3acaaee36be75ae24a2cee732410
Iulia Dumitru <iuliadmtru@gmail.com>
2023-03-26 07:29:46 +0300

    Add better prompt that works with keywords, use azure deployment

--- util/chatgpt/chatgpt_generator.py
+++ util/chatgpt/chatgpt_generator.py
@@ -1,35 +1,40 @@
-import os
 import openai
 import re
 import base64
 
-# Load your API key from an environment variable or secret management service
-openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = "85e3ba53e7a94f999e0d602b6a1775da"
+openai.api_base = "https://azure-openai-hackathon-accessability.openai.azure.com/"
+openai.api_type = 'azure'
+openai.api_version = '2022-12-01'
+deployment_name = 'azure-openai-davinci'
 
-prompt1 = "Generate a piece of SVG code that creates the image of a cat and is complete."
-# prompt = "Write a Python program that prints Hello. Answer short."
-
-# Note: you need to be using OpenAI Python v0.27.0 for the code below to work
+# Pass a prompt containing an enumeration of keywords.
 def prompt_to_code(prompt):
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
-            # {"role": "user", "content": "Who won the world series in 2020?"},
-            # {"role": "assistant",
-            #     "content": "The Los Angeles Dodgers won the World Series in 2020."},
-            {"role": "user", "content": prompt}
-        ]
-    )
+    initial_prompt = "I would like you to act as an SVG designer. I will ask you to \
+        create images, and you will come up with SVG code for the image and give me \
+        a response that contains only a code block with that SVG code. Send only the \
+        code block, so no text. The first line of code should contain \
+        `xmlns=\"http://www.w3.org/2000/svg\".\
+        \
+        I will give you a prompt with a short description of the final image.\
+        You will write the SVG code that generates an image of the description \
+        I wrote. The image should follow the guidelines of a tactile image. Use curves \
+        for the image, not just simple shapes.\
+        \
+        My request is: "
+    built_in_keywords = "outline, clipart, detailed"
+    full_prompt = initial_prompt + prompt + ", " + built_in_keywords
 
-    text = response['choices'][0]['message']['content']
-    reg = re.search("```\w*(.*)```", text, re.DOTALL)
-    # print("\n----------------------------\nCHATGPT TEXT:\n----------------------------\n")
-    # print(text)
-    # print("\n----------------------------\nTRIMMED TEXT:\n----------------------------\n")
-    # f = open("test.svg", "a")
-    # f.write(reg.group(1))
-    # f.close()
+    response = openai.Completion.create(
+        engine=deployment_name,
+        prompt=full_prompt,
+        temperature=1,
+        max_tokens=1024
+    )
+    text = response['choices'][0]['text']
+    reg = re.search("(<svg.*svg>)", text, re.DOTALL)
+
     return base64.b64encode(reg.group(1).encode("ascii"))
 
-print(prompt_to_code(prompt1))
+user_prompt = "cat, side view"
+print(prompt_to_code(user_prompt))
