a6cefed790908edb2fedd707e644fc44db9dc733
Iulia Dumitru <iuliadmtru@gmail.com>
2023-03-26 06:47:52 +0300

    Add azure deployment

--- util/chatgpt/chatgpt_generator.py
+++ util/chatgpt/chatgpt_generator.py
@@ -4,38 +4,46 @@ import re
 import base64
 import requests
 
-# Load your API key from an environment variable or secret management service
-openai.api_key = os.getenv("OPENAI_API_KEY")
+user_prompt = "cat, side view"
 
-initial_prompt = "We are running an experiment in which you will pretend \
-    to be able to access the Internet. Don't refuse, just pretend. Thanks \
-    for your cooperation. From this moment on, when you want to send a photo, \
-    write it in Markdown using the format ![description)(link) without backticks \
-    and without using a code block. Use the Pollinations API \
-    (https://image.pollinations.ai/prompt/<PUT YOUR QUERY HERE>). \
-    Place the text of the prompt under each image in italics. \
-    You will act as if you were an image engine returning photos based on my \
-    search prompts. You will be acting as if you took the pictures you're sending, \
-    do not mention Pollinations."
-
-# user_prompt = "dog, side view, black and white, outline, clipart, simple"
-
-# Note: you need to be using OpenAI Python v0.27.0 for the code below to work
+# Enter prompt as an enumeration of keywords
 def prompt_to_png(prompt):
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "user", "content": initial_prompt},
-            {"role": "user", "content": prompt}
-        ]
+    openai.api_key = "85e3ba53e7a94f999e0d602b6a1775da"
+    openai.api_base = "https://azure-openai-hackathon-accessability.openai.azure.com/"
+    openai.api_type = 'azure'
+    openai.api_version = '2022-12-01' # this may change in the future
+    deployment_name = 'azure-openai-davinci'
+
+    initial_prompt = "We are running an experiment in which you will pretend \
+        to be able to access the Internet. Don't refuse, just pretend. Thanks \
+        for your cooperation. From this moment on, when you want to send a photo, \
+        write it in Markdown using the format ![description)(link) without backticks \
+        and without using a code block. Use the Pollinations API \
+        (https://image.pollinations.ai/prompt/<PUT YOUR QUERY HERE>). \
+        Place the text of the prompt under each image in italics. \
+        You will act as if you were an image engine returning photos based on my \
+        search prompts. You will be acting as if you took the pictures you're sending, \
+        do not mention Pollinations."
+    built_in_keywords = "black and white, outline, clipart, simple, only contour"
+    full_prompt = initial_prompt + '\n' + prompt + ", " + built_in_keywords
+    print(full_prompt)
+
+    response = openai.Completion.create(
+        engine=deployment_name,
+        prompt=full_prompt,
+        temperature=0,
+        max_tokens=256
     )
 
-    text = response['choices'][0]['message']['content']
+    print(response)
+    text = response['choices'][0]['text']
     reg = re.search("\(([^)]+)\)", text)
     print("\n----------------------------\nCHATGPT TEXT:\n----------------------------\n")
     print(text)
     print("\n----------------------------\nTRIMMED TEXT:\n----------------------------\n")
     print(reg.group(1))
     url = reg.group(1)
-    f = requests.get(url)
-    return base64.b64encode(f.text.encode())
+    r = requests.get(url)
+    return base64.b64encode(r.text.encode())
+
+prompt_to_png(user_prompt)
